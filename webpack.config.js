e454a9670e34cb977fe387cde1301d15a42602ee
Hader <hader.araujo@gmail.com>
2016-11-11 10:41:40 +0000

    change to run in c9.io

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,10 @@ var path = require('path');
 module.exports = {
   context: path.join(__dirname, "src"),
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./js/client.js",
+  entry: [
+         'webpack-dev-server/client?https://0.0.0.0:8080',
+        './js/client.js'
+    ],
   module: {
     loaders: [
       {
